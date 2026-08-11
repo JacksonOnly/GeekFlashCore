528030dc038ee1ad744a28e47d8dd6461f0911a9
XYCS <213067999+JacksonOnly@users.noreply.github.com>
2026-08-12 07:54:42 +0800

    ✨ feat(Transport.Abstractions): 添加ITransport

---
+++ src/GeekFlashCore.Transport.Abstractions/Interfaces/IControlTransferTransport.cs
@@ -0,0 +1,18 @@
+﻿namespace GeekFlashCore.Transport.Abstractions;
+
+public interface IControlTransferTransport
+{
+    void ControlOut(
+        byte requestType,
+        byte request,
+        ushort value,
+        ushort index,
+        ReadOnlySpan<byte> data);
+
+    int ControlIn(
+        byte requestType,
+        byte request,
+        ushort value,
+        ushort index,
+        Span<byte> destination);
+}
---
+++ src/GeekFlashCore.Transport.Abstractions/Interfaces/ITransport.cs
@@ -0,0 +1,15 @@
+﻿namespace GeekFlashCore.Transport.Abstractions;
+
+public interface ITransport : IDisposable
+{
+    bool IsOpen { get; }
+    void Open();
+    void Close();
+    void Write(ReadOnlySpan<byte> data, int? timeoutInMilliseconds = null);
+    void Write(byte[] data, int offset, int count, int? timeoutInMilliseconds = null);
+    int Read(Span<byte> data, int? timeoutInMilliseconds = null);
+    int Read(byte[] data, int offset, int count, int? timeoutInMilliseconds = null);
+    int ReadExact(Span<byte> destination, int? timeoutInMilliseconds = null);
+    int ReadAvailable(Span<byte> data);
+    void Flush();
+}

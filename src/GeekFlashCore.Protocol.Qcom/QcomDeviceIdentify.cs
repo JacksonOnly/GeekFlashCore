a29e8494c7d088db7cae278479879a33d8fa52f2
XYCS <213067999+JacksonOnly@users.noreply.github.com>
2026-08-13 19:45:00 +0800

    ✨ feat(Protocol.Qcom): 实现QcomDeviceIdentify

---
+++ src/GeekFlashCore.Protocol.Qcom/QcomDeviceIdentify.cs
@@ -0,0 +1,21 @@
+﻿using GeekFlashCore.Protocol.Abstractions;
+using GeekFlashCore.UsbWatcher.Abstractions;
+
+namespace GeekFlashCore.Protocol.Qcom;
+
+public class QcomDeviceIdentify : IDeviceIdentify
+{
+    public DeviceProbeResult Identify(UsbDeviceInfo deviceInfo)
+    {
+        ProtocolType protocolType = (deviceInfo.VendorId, deviceInfo.ProductId) switch
+        {
+            // 暂时就直接这样子了，之后会添加的。。。
+            (0x05c6,_) => ProtocolType.QualcommEdl,
+            _ => ProtocolType.Unknown
+        };
+        if(protocolType!= ProtocolType.Unknown)
+            return DeviceProbeResult.Ok(protocolType);
+        else 
+            return DeviceProbeResult.Fail();
+    }
+}

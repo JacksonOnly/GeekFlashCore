8b2629eea7ba764f5adef0ea9ba747b64320871b
XYCS <213067999+JacksonOnly@users.noreply.github.com>
2026-08-14 13:08:01 +0800

    ✨ feat(Qcom.Abstractions): 添加两个协议的异常类

---
+++ src/GeekFlashCore.Protocol.Qcom.Abstractions/FirehoseProtocolException.cs
@@ -0,0 +1,16 @@
+﻿namespace GeekFlashCore.Protocol.Qcom.Abstractions;
+
+public class FirehoseProtocolException : QcomProtocolException
+{
+    public FirehoseProtocolException()
+    {
+    }
+
+    public FirehoseProtocolException(string? message) : base(message)
+    {
+    }
+
+    public FirehoseProtocolException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ src/GeekFlashCore.Protocol.Qcom.Abstractions/SaharaProtocolException.cs
@@ -0,0 +1,16 @@
+﻿namespace GeekFlashCore.Protocol.Qcom.Abstractions;
+
+public class SaharaProtocolException : QcomProtocolException
+{
+    public SaharaProtocolException()
+    {
+    }
+
+    public SaharaProtocolException(string? message) : base(message)
+    {
+    }
+
+    public SaharaProtocolException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
